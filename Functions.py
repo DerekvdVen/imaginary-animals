7478bc989d0653ef575b46782be66aa9106d8154
Derek van de Ven <derek.vandeven@wur.nl>
2019-10-23 18:05:54 +0200

    added border function to find centers instead of regmax which works way way better

--- Functions.py
+++ Functions.py
@@ -27,11 +27,26 @@ def create_dirs():
         print ("Successfully created the directory %s " % path_labels)
         print ("Successfully created the directory %s " % path_semantic)
         
+def check_sky(input_image_s):
+    img = cv2.imread(input_image_s)
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv_color_sky1 = np.asarray([59, 177, 180])   # sky
+    hsv_color_sky2 = np.asarray([61, 179, 182])   # 
+
+    check_sky = cv2.inRange(img_hsv, hsv_color_sky1, hsv_color_sky2)
+    if 255 in check_sky:
+        print("Warning, picture contains sky, passing")
+        return True
+    else:
+        return False
+
 def mask_seg(imagelocation):
     
     # Convert to HSV
     img = cv2.imread(imagelocation)
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    # Test if sky is in image
 
     # Set exclusion parameters for ground, vegetation and water
     hsv_color_ground1 = np.asarray([50, 210, 224])   # exclude ground
@@ -54,8 +69,8 @@ def mask_seg(imagelocation):
     target = cv2.bitwise_and(img_hsv,img_hsv, mask=mask)
 
     # Display image
-    plt.imshow(target)   # this colormap will display in black / white
-    plt.show()
+    #plt.imshow(target)   # this colormap will display in black / white
+    #plt.show()
     animals = cv2.bitwise_not(mask)
     
     return animals, mask
@@ -70,45 +85,95 @@ def smooth_animals(animals, sigma, size):
     animalsf = mh.gaussian_filter(animals, sigma)
     animalsf_I = animalsf.astype('uint8')
     T = mh.thresholding.otsu(animalsf_I)
-    pylab.imshow(animalsf)
-    pylab.show()
+    #pylab.imshow(animalsf)
+    #pylab.show()
     
     return animalsf, T
 
 def count_animals(animals_smooth, T,min_size = 1):
+    
+    # Smooth animals
     labeled, nr_objects = mh.label(animals_smooth > T)
     
-    print(nr_objects)
-    #sizes = mh.labeled.labeled_size(labeled)
-    #too_small = np.where(sizes < min_size)
-    #labeled_filtered = mh.labeled.remove_regions(labeled, too_small)
+    print("This image contains" , nr_objects, "animals")
     
-    #labeled, nr_objects = mh.label(labeled_filtered)
-    
-    print("This image contains " , nr_objects, "animals")
-    pylab.imshow(labeled)
-    pylab.jet()
-    pylab.show()
-    
+    # plot if animals present
+    if nr_objects != 0:
+        pylab.imshow(labeled)
+        pylab.jet()
+        pylab.show()
+
     return labeled, nr_objects
 
+def get_centers_through_borders(labeled,nr_objects,width = 512, height = 512):
+    centers_list = []
+    for x in range(nr_objects):
+        
+        location = np.where(mh.borders(labeled == x))
+        x_location = np.mean(location[1])/width
+        y_location = np.mean(location[0])/height
+        centers_list.append((x_location,y_location))
+    print(centers_list)
+    return(centers_list)
+
+def get_bboxes(labeled, width = 512, height = 512):
+    bboxes = mh.labeled.bbox(labeled)
+    bbox_list = []
+    
+    for box in bboxes[1:]:
+        bbox_list.append((abs(box[2] - box[3])/width, abs(box[0] - box[1])/height))
+        
+    return(bbox_list)
+
+def write_file(output_location,image_name,centers_list,bbox_list):
+    bad_image_list1 = []
+    output_file = output_location + image_name + ".txt"
+    with open(output_file, "w") as file:
+        for wh, xy in zip(centers_list, bbox_list):
+            if len(centers_list) != len(bbox_list):
+                print("Warning: Skipped image, n centers not equal to n animals")
+                bad_image_list1.append(image_name)
+                break
+
+            file.write("1" +  ' ' + str(wh[0]) + ' ' + str(wh[1]) + ' ' + str(xy[0]) + ' ' + str(xy[1]))
+            file.write("\n")
+    return(bad_image_list1)
+
+# old stuff
+'''
 def get_centers(animals_smooth, clean_distance, width = 512, height = 512):
     rmax = mh.regmax(animals_smooth)
     centers, nr_centers = mh.label(rmax)
     print("nr centers", nr_centers)
     
-    pylab.imshow(animals_smooth)
-    pylab.jet()
-    pylab.show()
-    
+    # plot if animals present
+    if nr_centers != 0:
+        pylab.imshow(animals_smooth)
+        pylab.jet()
+        pylab.show()
+
     centers_list = []
     centers_list_clean = []
     for x in range(nr_centers):
         location = np.where(centers == (x+1))
-        x_location = float(location[1])/width
-        y_location = float(location[0])/height
+        print(location)
+        
+        # if center location is more than one pixel,
+        if len(location[0])>1:
+            x_location = float(np.mean(location[1]))/width ### check if mean works here, it should
+            print("location is big")
+        else:
+            x_location = float(location[1])/width
+            
+        if len(location[1])>1:
+            y_location = float(np.mean(location[0][0]))/height
+            print("big center, refining")
+        else:
+            y_location = float(location[0])/height
+            
         centers_list.append((x_location,y_location))
     
+    # This checks if centers are not too close together
     for x in range(0,len(centers_list)-1):        
         if abs(centers_list[x][0] - centers_list[x+1][0]) < clean_distance/width and abs(centers_list[x][1]-centers_list[x+1][1]) < clean_distance/height:
             print("Multiple centers found, removing centers close together")
@@ -121,27 +186,4 @@ def get_centers(animals_smooth, clean_distance, width = 512, height = 512):
     print("nr centers", len(centers_list_clean))
     print(centers_list_clean)
     return centers_list_clean
-
-def get_bboxes(labeled, width = 512, height = 512):
-    bboxes = mh.labeled.bbox(labeled)
-    bbox_list = []
-    
-    for box in bboxes[1:]:
-        #print(abs(box[2] - box[3])/width, abs(box[0] - box[1])/height)
-        bbox_list.append((abs(box[2] - box[3])/width, abs(box[0] - box[1])/height))
-        
-    return(bbox_list)
-
-def write_file(output_location,image_name,centers_list,bbox_list):
-    bad_image_list = []
-    output_file = output_location + image_name + ".txt"
-    with open(output_file, "w") as file:
-        for wh, xy in zip(centers_list, bbox_list):
-            if len(centers_list) != len(bbox_list):
-                print("Warning: Skipped image, n centers not equal to n animals")
-                bad_image_list.append(image_name)
-                break
-
-            file.write("1" +  ' ' + str(wh[0]) + ' ' + str(wh[1]) + ' ' + str(xy[0]) + ' ' + str(xy[1]))
-            file.write("\n")
-    return(bad_image_list)
+'''
---
+++ cv_capture_random.py
@@ -0,0 +1,86 @@
+# In settings.json first activate computer vision mode: 
+# https://github.com/Microsoft/AirSim/blob/master/docs/image_apis.md#computer-vision-mode
+
+import setup_path 
+import airsim
+import pprint
+import tempfile
+import os
+import time
+import random
+import itertools
+
+pp = pprint.PrettyPrinter(indent=4)
+
+client = airsim.VehicleClient()
+
+airsim.wait_key('Press any key to get camera parameters')
+for camera_id in range(2):
+    camera_info = client.simGetCameraInfo(str(camera_id))
+    print("CameraInfo %d: %s" % (camera_id, pp.pprint(camera_info)))
+
+airsim.wait_key('Press any key to get images')
+#tmp_dir = os.path.join(tempfile.gettempdir(), "airsim_drone")
+tmp_dir = "C:/Users/Derek/Documents/Thesis_python/Data/images/2019-10"
+tmp_dir2 = "C:/Users/Derek/Documents/Thesis_python/Data/semantic/2019-10"
+print ("Saving images to %s" % tmp_dir)
+
+#try:
+#    for n in range(2):
+#        os.makedirs(os.path.join(tmp_dir, str(n)))
+#except OSError:
+#    if not os.path.isdir(tmp_dir):
+#        raise
+
+#client.simSetTimeOfDay(True, start_datetime = "2018-10-14 {}:12:00")
+Z = -(int(input("how high? (m)")))
+S = int(input("How big is the stepsize?"))
+px = 0 
+py = 0
+counter = 0
+while counter < 1000:
+    x1 = random.randrange(-1500,1500,S)
+    y1 = random.randrange(-1500,1500,S)
+    fill_value = x1
+    distance_y = abs(y1 - py)
+    distance_x = abs(x1 - px)
+    if distance_y < distance_x:
+        fill_value = y1
+    print("Flying to:",x1,y1)
+    print("current_location = ",px,",",py)
+    time.sleep(3)
+    S1,S2 = S, S
+    if x1 < px:
+        S1 = -S
+    if y1 < py:
+        S2 = -S
+    for x, y in itertools.zip_longest(range(px, x1, S1), range(py, y1, S2), fillvalue = fill_value): # do few times
+        counter += 1
+        #xn = 1 + x*5  # some random number
+        client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(x, y, Z), airsim.to_quaternion(-1.57079633, 0, 0)), True)
+        time.sleep(0.5)
+        #print("CameraInfo %d: %s" % (camera_id, pp.pprint(camera_info)))
+        print(x,'___',y)
+    
+        angles = airsim.to_eularian_angles(client.simGetVehiclePose().orientation)
+        #print("pitch={}, roll={}, yaw={}".format(angles[0], angles[1], angles[2]))
+        responses = client.simGetImages([
+            airsim.ImageRequest("0", airsim.ImageType.Scene)])
+        responses_s = client.simGetImages([
+            airsim.ImageRequest("0", airsim.ImageType.Segmentation)])
+
+        for i, response in enumerate(responses):
+            print("Type %d, size %d, pos %s" % (response.image_type, len(response.image_data_uint8), pprint.pformat(response.camera_position)))
+            airsim.write_file(os.path.normpath(os.path.join(tmp_dir,  "IMG" + str(counter) + '_' + str(x) + ',' + str(y) + '_' + '.png')), response.image_data_uint8)
+        for i, response in enumerate(responses_s):
+            print("Type %d, size %d, pos %s" % (response.image_type, len(response.image_data_uint8), pprint.pformat(response.camera_position)))
+            airsim.write_file(os.path.normpath(os.path.join(tmp_dir2,  "IMG" + str(counter) + '_' + str(x) + ',' + str(y) + '_' + '.png')), response.image_data_uint8)
+            
+        pose = client.simGetVehiclePose()
+        #pp.pprint(pose)
+    px = x1
+    py = y1
+    time.sleep(3)
+
+# currently reset() doesn't work in CV mode. Below is the workaround
+client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(0, 0, 0), airsim.to_quaternion(-1.57079633, 0, 0)), True)
