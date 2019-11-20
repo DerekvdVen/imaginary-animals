2b6a6abd605343d3974c063aa40553b7aef8b420
Ven <ven071@guanabana.wurnet.nl>
2019-11-20 12:44:35 +0100

    loading sortof works now

--- test_faster_rcnn.py
+++ test_faster_rcnn.py
@@ -10,26 +10,8 @@ import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.rpn import AnchorGenerator
-
-#img = mpimg.imread('../Data/images/2019-11/IMG44_280,-80.png')
-
-img = Image.open('../Data/images/2019-11/IMG58_70,130.png')
-mask = Image.open('../Data/masks/2019-11/IMG58_70,130.png')
-
-mask.putpalette([
-    0, 0, 0, # black background
-    255, 0, 0, # index 1 is red
-])
-#imgmask = np.array(mask)
-print(mask)
-
-
-#plt.imshow(mask)
-#plt.show()
-
-#plt.imshow(img)
-#plt.show()
-#root = ../Data
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 class Animals_dataset(torch.utils.data.Dataset):
     def __init__(self, root, transforms=None):
@@ -100,10 +82,26 @@ class Animals_dataset(torch.utils.data.Dataset):
         return len(self.imgs)
 
 
-dataset = Animals_dataset('../Data/')
-print(dataset[0])
+def get_instance_segmentation_model(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
 
-# load a model pre-trained pre-trained on COCO
+    # get the number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
+    return model
+
+
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
 
@@ -151,72 +149,6 @@ model = FasterRCNN(backbone,
                    box_roi_pool=roi_pooler)
 
 
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
-
-def get_instance_segmentation_model(num_classes):
-    # load an instance segmentation model pre-trained on COCO
-    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-
-    # get the number of input features for the classifier
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-    # now get the number of input features for the mask classifier
-    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    hidden_layer = 256
-    # and replace the mask predictor with a new one
-    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
-                                                       hidden_layer,
-                                                       num_classes)
-
-    return model
-
-from engine import train_one_epoch, evaluate
-import utils
-import transforms as T
-
-
-def get_transform(train):
-    transforms = []
-    # converts the image, a PIL image, into a PyTorch Tensor
-    transforms.append(T.ToTensor())
-    if train:
-        # during training, randomly flip the training images
-        # and ground-truth for data augmentation
-        transforms.append(T.RandomHorizontalFlip(0.5))
-    return T.Compose(transforms)
-
-# use our dataset and defined transformations
-dataset = Animals_dataset('../Data/', get_transform(train=True))
-dataset_test = Animals_dataset('../Data/', get_transform(train=False))
-
-# split the dataset in train and test set
-torch.manual_seed(1)
-indices = torch.randperm(len(dataset)).tolist()
-dataset = torch.utils.data.Subset(dataset, indices[:-25])
-dataset_test = torch.utils.data.Subset(dataset_test, indices[-25:])
-
-# define training and validation data loaders
-data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=2, shuffle=True, num_workers=4,
-    collate_fn=utils.collate_fn)
-
-data_loader_test = torch.utils.data.DataLoader(
-    dataset_test, batch_size=1, shuffle=False, num_workers=4,
-    collate_fn=utils.collate_fn)
-
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-# our dataset has two classes only - background and person
-num_classes = 2
-
-# get the model using our helper function
-model = get_instance_segmentation_model(num_classes)
-# move model to the right device
-model.to(device)
-
 # construct an optimizer
 params = [p for p in model.parameters() if p.requires_grad]
 optimizer = torch.optim.SGD(params, lr=0.005,
@@ -227,6 +159,28 @@ optimizer = torch.optim.SGD(params, lr=0.005,
 lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
                                                step_size=3,
                                                gamma=0.1)
+
+
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# our dataset has two classes only - background and person
+num_classes = 2
+
+# get the model using our helper function
+model = get_instance_segmentation_model(num_classes)
+
+# move model to the right device
+#model.to(device)
+
+
+PATH = '../models/faster_rcnn.pth'
+#model = Net()
+model.load_state_dict(torch.load(PATH))
+
+model.eval()
+
+
 
 # Print model's state_dict
 print("Model's state_dict:")
@@ -239,21 +193,6 @@ for var_name in optimizer.state_dict():
     print(var_name, "\t", optimizer.state_dict()[var_name])
 
 
-# let's train it for 10 epochs
-num_epochs = 2
-
-for epoch in range(num_epochs):
-    # train for one epoch, printing every 10 iterations
-    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
-    # update the learning rate
-    lr_scheduler.step()
-    # evaluate on the test dataset
-    evaluate(model, data_loader_test, device=device)
-
-PATH = '../models/faster_rcnn.pth'
-
-torch.save(model.state_dict(), PATH)
-
 # pick one image from the test set
 img, _ = dataset_test[0]
 # put the model in evaluation mode
@@ -263,6 +202,12 @@ with torch.no_grad():
 
 print(prediction)
 
+
+# use our dataset and defined transformations
+dataset = Animals_dataset('../Data/', get_transform(train=True))
+dataset_test = Animals_dataset('../Data/', get_transform(train=False))
+
+Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy())
 img = Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy())
 plt.imshow(img)
 plt.show()
