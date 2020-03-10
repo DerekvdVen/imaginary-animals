2e378ff82d5b9ad04cf8d129f49de3c57a165802
Derek <derekvdven@gmail.com>
2020-03-10 14:27:51 +0100

    backup

--- array_build.sh
+++ array_build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # run mulitple models over columns. Output jsons are saved in pytorch_retinanet/calcmeanap/[run_name].json
-run_name_array=(t1_30m_ea_false t2_60m t3_6030m)     # name of run
-dir_array=(30m 60m 60m30m)                  # data directory
+run_name_array=(t1_30m_ea_false t2_60m t3_6030m)    # name of run
+dir_array=(30m 60m 60m30m)                          # data directory
 
-ea_array=(' ' ea ea)                         # save empty animals to train val test list.txts
+ea_array=(' ' ea ea)                                # save empty animals to train val test list.txts
 
-lr_array=(1e-5 1e-5 1e-5)                   # learning rate
-bs_array=(4 4 4)                            # batch size
-ne_array=(50 50 50)                         # number of epochs
+lr_array=(1e-5 1e-5 1e-5)                           # learning rate
+bs_array=(4 4 4)                                    # batch size
+ne_array=(50 50 50)                                 # number of epochs
 
-mc_array=(0.2 0.2 0.2)                      # minimum confidence for predicting 
-iou_array=(0.2 0.2 0.2)                     # non maximal supression iou
+mc_array=(0.2 0.2 0.2)                              # minimum confidence for predicting 
+iou_array=(0.2 0.2 0.2)                             # non maximal supression iou
 
 
 for x in 0 1
@@ -25,25 +25,25 @@ ne_x="-ne ${ne_array[$x]}"
 mc_x="-mc ${mc_array[$x]}"
 iou_x="-nms_iou ${iou_array[$x]}"
 
-
 ######################################################## Creating annotations
 # # check for animals takes images from Data/all/60m|30m and moves the animal images into Data/only_animal_images/all/60m|30m
 # echo check all images for animals
 # python check_for_animals.py $dir_x
 
 # # takes input from Data/all/60m|30m and splits the data into train and test groups in Data/semantic|images/60|30m/ 
-# echo creating training and validation sets
-# python split_train_test.py $dir_x
+# # make sure to rerun this one when using different distances in dir_x array
+echo creating training and validation sets
+python split_train_test.py $dir_x
 
 # # takes input from Data/semantic|images/60|30m/ and creates annotations in a train.txt and test.txt file 
-#echo run main.py with train/test distributions
-#python main.py $run_name_x $ea_x
+echo run main.py with train/test distributions
+python main.py $run_name_x $ea_x
 
 ######################################################### CNN
 # runs the CNN with data from Data/all/60|30m and annotations from train.txt and test.txt
 echo training retinanet
 cd pytorch_retinanet
-python train.py $run_name_x $lr_x $bs_x $ne_x $dir_x
+python train.py $run_name_x $lr_x $bs_x $ne_x $dir_x $ea_x
 
 # check model output with test.py or val.py
 python test.py $run_name_x $mc_x $iou_x
--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-run_name="-n test60"       # name of run
-dir="-dir 60m"            # data directory
+run_name="-n 20304060m"       # name of run
+dir="-dir 2346m"            # data directory
 
 ea="-ea"                  # save empty animals to train val test list.txts
 
 lr="--lr 1e-5"            # learning rate
-bs="-bs 1"                # batch size
-ne="-ne 1"                # number of epochs
+bs="-bs 4"                # batch size
+ne="-ne 50"                # number of epochs
 
 mc="-mc 0.2"              # minimum confidence for predicting
 iou="-nms_iou 0.2"        # non maximal supression iou
@@ -25,14 +25,14 @@ iou="-nms_iou 0.2"        # non maximal supression iou
 # python main.py $run_name $ea
 
 # # runs the CNN with data from Data/all/60|30m and annotations from train.txt and test.txt
-# echo training retinanet
+echo training retinanet
 cd pytorch_retinanet
-python train.py $run_name $lr $bs $ne $dir
+python train.py $run_name $lr $bs $ne -r #$ni
 
-# # check model output with test.py or val.py
+# # check model output on rendered images
 # echo testing on rendered images
 # python test.py $run_name $mc $iou
 
-# # python val.py 
+# python val.py check model output on real images
 # echo testing on real images
 # python val.py $run_name $mc $iou -r
