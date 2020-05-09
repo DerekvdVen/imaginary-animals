14d01b84384d8146790acb7bba3a1f04e1996d88
Derek <derekvdven@gmail.com>
2020-05-09 11:48:23 +0200

    final scripts

--- pytorch_retinanet/fine_tune.sh
+++ pytorch_retinanet/fine_tune.sh
@@ -2,10 +2,10 @@
 
 # this code will run the model on a small training set of x real images
 
-run_name="-n 23456z3_2500"                              # name of run
-ni="-ni 0"                                                 # number of images
-#cp ./checkpoint/20304060m_z.pth ./checkpoint/20304060m_1000f.pth #(only do this for finetuning)
-renim="-renim 2500"
+run_name="-n 23456z3_2000e2"                              # name of run
+ni="-ni 2000"                                                 # number of images
+#cp ./checkpoint/23456z3_looksgood.pth ./checkpoint/23456z3_250fin2.pth #(only do this for finetuning)
+renim="-renim 0"                                               #number of rendered images
 
 dir="-dir 23456m_np"         # 20304060m_np     np means new parameters
 perc="-p 10"
@@ -15,7 +15,7 @@ ne="-ne 20"                                                 # number of epochs
 
 r="" #!!!!!!!!!!!!                  (fine tuning turn on)
 
-mc="-mc 0.1"                                                # minimum confidence for predicting
+mc="-mc 0.01"                                                # minimum confidence for predicting
 iou="-nms_iou 0.2"                                          # non maximal supression iou
 
 
---
+++ pytorch_retinanet/testmodels.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# python test.py -n 23456z3_1250
+# python test.py -n 23456z3_2500_end_good
+# python test.py -n 23456z3_7500n
+# python test.py -n 23456z3_10000n
+
+# python test.py -n 20304060m_250e  
+
+# python test.py -n 23456z3_250fin  
+ 
+# python test.py -n 20304060m_500e  
+ 
+# python test.py -n 23456z3_500fin  
+
+# python test.py -n 20304060m_1000e  
+ 
+python test.py -n 23456z3_2000fin  
+
+
+#python test.py -n 23456z3_2000mix
+
+# python test.py -n 23456z3_1000mix
+
+# python test.py -n 23456z3_500mix
+
+# python test.py -n 23456z3_250mix
